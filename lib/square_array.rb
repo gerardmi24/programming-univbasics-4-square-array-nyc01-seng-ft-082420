e3feb453cf7ed7f6947db83494c6148f00003400
Gerard Mistretta <gerardmistretta@yahoo.com>
2020-07-29 00:03:37 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,8 @@
-integers = [5, 10, 12]
-new_integers = []
+array = [5, 10, 12]
+new_array = []
 def square_array(array)
-  while do # your code here
-
-
-
+  array.times do |index|
+    new_array.push(array[index] + 1)
+     # your code here
 end
+new_array
