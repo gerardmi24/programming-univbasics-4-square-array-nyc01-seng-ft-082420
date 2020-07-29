bfca0132a58e1f57671e7ae834149e76581c64bd
Gerard Mistretta <gerardmistretta@yahoo.com>
2020-07-29 00:09:01 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ numbers = [1, 2, 3]
 new_numbers = []
 def square_array(numbers)
   numbers.times do |index|
-    new_array.push(array[index] + 1)
+    new_array.push(array[index] + 1)**
      # your code here
 end
 new_array
